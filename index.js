47ab111d597e238df324673e21e336ecd409b1da
rohanjain3 <124330771+rohanjain3@users.noreply.github.com>
2023-05-16 22:45:06 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,27 @@
+{/* <div class="form-box login">
+                <form action="">
+                    <h1>Sign In</h1>
+                   <div class="input-box">
+                    <span class="icon"><i class="bx bxs-envelope"></i></span>
+                    <!-- <input type="email" required>
+                    <label for="Email">Email</label> -->
+                    <input type="email" required>
+                    <label for="email">Email</label>
+                   </div>
+                   <div class="input-box">
+                    <span class="icon"><i class="bx bxs-lock-alt"></i></span>
+                    <input type="password" required>
+                    <label for="Password">Password</label>
+                   </div>
+                   <div class="remenber-password">
+                    
+                    <label for=""><input type="checkbox">Remember Me</label>
+                    <a href="#">Forgot Password</a>
+                   </div>
+                   <br>
+                   <button class="btn">Loge In</button>
+                   <div class="create-account">
+                    <p>Create A New Account? <a href="#" class="register-link">Sign Up</a></p>
+                   </div>
+                </form>
+            </div> */}
